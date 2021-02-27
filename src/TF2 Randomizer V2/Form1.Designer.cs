cb9d6b8ed366825fea1cbb1263ae99aba4320098
romdotdog <70765447+romdotdog@users.noreply.github.com>
2021-02-27 17:39:53 -0600

    Commit project

---
+++ src/TF2 Randomizer V2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TF2_Randomizer_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.check1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 120);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 181);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Application launched.\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Seed";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Pick A Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(214, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generate VPK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // check1
+            // 
+            this.check1.AutoSize = true;
+            this.check1.Location = new System.Drawing.Point(119, 42);
+            this.check1.Name = "check1";
+            this.check1.Size = new System.Drawing.Size(72, 17);
+            this.check1.TabIndex = 5;
+            this.check1.Text = "Weapons";
+            this.check1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.check1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 313);
+            this.Controls.Add(this.check1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox check1;
+    }
+}
+
---
+++ src/TF2 Randomizer V2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TF2_Randomizer_V2
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+
+        string weapons = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text.GetHashCode().ToString();
+            r = new Random(textBox1.Text.GetHashCode());
+        }
+
+        private void add_log(string text)
+        {
+            richTextBox1.Text = richTextBox1.Text + text + "\n";
+        }
+
+        private void DeleteDirectory(string target_dir)
+        {
+            string[] files = Directory.GetFiles(target_dir);
+            string[] dirs = Directory.GetDirectories(target_dir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(target_dir, true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dr = folderBrowserDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                add_log("\"" + folderBrowserDialog1.SelectedPath + "\" added to variable \"PATH\"");
+                if (!Directory.Exists(folderBrowserDialog1.SelectedPath + @"\weapons"))
+                {
+                    add_log("[Directory Protection]: This directory is invalid. needs a weapons subdirectory");
+                    button2.Enabled = false;
+                } else
+                {
+                    if (Directory.Exists(Path.GetTempPath() + @"\tf2random"))
+                    {
+                        add_log("[Directory Protection]: Deleting unneeded randomizations from last time..");
+                        DeleteDirectory(Path.GetTempPath() + @"\tf2random");
+                    }
+                    add_log("[Directory Protection]: All clear.");
+                    weapons = folderBrowserDialog1.SelectedPath + @"\weapons";
+                    button2.Enabled = true;
+                }
+            }
+            
+        }
+
+        private void KeyPressedCancel(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.KeyPress += KeyPressedCancel;
+            textBox2.Text = r.Next(-2147483647, 2147483647).ToString();
+        }
+
+        private string GetRandom(List<string> directories)
+        {
+            var result = directories[r.Next(0, directories.Count)];
+            return result;
+        }
+
+        private void CopyDirectory(string SourcePath, string DestinationPath)
+        {
+            try
+            {
+                foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+        SearchOption.AllDirectories))
+                    File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+            }
+            catch
+            {
+                add_log("Something went wrong..");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Directory.CreateDirectory(Path.GetTempPath() + @"\tf2random");
+            Directory.CreateDirectory(Path.GetTempPath() + @"\tf2random\models");
+            var actualtemp = Directory.CreateDirectory(Path.GetTempPath() + @"\tf2random\models\weapons").FullName;
+
+            if (check1.Checked)
+            {
+                add_log("Logging Directories..");
+                var directories = Directory.GetDirectories(weapons + @"\c_models").ToList();
+                var directories2 = Directory.GetDirectories(weapons + @"\c_models");
+                var temp = Directory.CreateDirectory(actualtemp + @"\c_models").FullName;
+                foreach (string weeapon in directories2)
+                {
+                    var opponent = GetRandom(directories);
+                    directories.Remove(opponent);
+                    var previousname = Path.GetFileName(weeapon);
+                    var newname = Path.GetFileName(opponent);
+                    var weapon = Directory.CreateDirectory(temp + @"\" + newname).FullName;
+                    CopyDirectory(weeapon, weapon);
+                    add_log("Doing " + previousname + " -> " + newname);
+                    foreach (string file in Directory.GetFiles(weapon))
+                    {
+                        add_log(Path.GetFileName(file) + " -> " + Path.GetFileName(file).Replace(previousname, newname));
+                        var newpath = Path.GetDirectoryName(file) + @"\" + Path.GetFileName(file).Replace(previousname, newname); //On the safe side instead of replacing the entire path
+                        File.Move(file, newpath);
+                    }
+                }
+
+                //TODO: v_models
+                add_log("-------------------------\n\n-------------------------");
+                add_log("2nd stage: Logging Directories");
+                var namefiles = Directory.GetFiles(weapons + @"\v_models", "*.mdl").ToList();
+                var namefiles2 = Directory.GetFiles(weapons + @"\v_models", "*.mdl");
+                temp = Directory.CreateDirectory(actualtemp + @"\v_models").FullName;
+                foreach (string namefile in namefiles2)
+                {
+                    var opponent = GetRandom(namefiles);
+                    var previousname = Path.GetFileName(namefile);
+                    var newname = Path.GetFileName(opponent);
+                    var files_of_namefile = Directory.GetFiles(weapons + @"\v_models", Path.GetFileNameWithoutExtension(namefile) + "*.*");
+                    foreach(string file_of_namefile in files_of_namefile)
+                    {
+                        add_log(Path.GetFileName(file_of_namefile) + " -> " + Path.GetFileName(file_of_namefile).Replace(previousname, newname));
+                        File.Copy(file_of_namefile, temp + @"\" + Path.GetFileName(file_of_namefile).Replace(previousname, newname), true);
+                    }
+                }
+
+                //TODO: w_models
+                add_log("-------------------------\n\n-------------------------");
+                add_log("3rd stage: Logging Directories");
+                namefiles = Directory.GetFiles(weapons + @"\w_models", "*.mdl").ToList();
+                namefiles2 = Directory.GetFiles(weapons + @"\w_models", "*.mdl");
+                foreach (string namefile in namefiles2)
+                {
+                    var opponent = GetRandom(namefiles);
+                    var previousname = Path.GetFileName(namefile);
+                    var newname = Path.GetFileName(opponent);
+                    var files_of_namefile = Directory.GetFiles(weapons + @"\w_models", Path.GetFileNameWithoutExtension(namefile) + "*.*");
+                    foreach (string file_of_namefile in files_of_namefile)
+                    {
+                        add_log(Path.GetFileName(file_of_namefile) + " -> " + Path.GetFileName(file_of_namefile).Replace(previousname, newname));
+                        File.Copy(file_of_namefile, temp + @"\" + Path.GetFileName(file_of_namefile).Replace(previousname, newname), true);
+                    }
+                }
+            }
+            add_log("Done");
+        }
+    }
+}
